a1ae546dc3d3f57af9284549dc6fcd3650af1f20
ahkh9 <100486910+ahkh9@users.noreply.github.com>
2022-10-06 21:23:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function cat1() {
   let cate = [];
   const name = [];
   let content = '';
+  let cate1=[];
   for (var i = 0; i <= 9; i++) {
 
     cate[i] = document.querySelectorAll(".btncate")[i].text.slice(0);
@@ -44,7 +45,7 @@ function cat1() {
 
     if (cate[i].charAt(8) === Catogrey1.charAt(8)) {
 
-      
+      cate1[c]=cate[i];
       name[c] = document.querySelectorAll(".name")[i].textContent.slice(0);
       // console.log(name[i]);
       // console.log(cate[i]);
@@ -63,7 +64,7 @@ console.log(name.length);
     content += `
     <div class="card" style="width: 25rem;margin:0 auto;top:6em;">
 <ul class="list-group list-group-flush">
-<li class="list-group-item"><span class="circle">${firstLetters}</span> <span class="name">  ${name[i]}   </span><a href="#" class="btn btn-primary btncate">Catogrey1 </a> 
+<li class="list-group-item"><span class="circle">${firstLetters}</span> <span class="name">  ${name[i]}   </span><a href="#" class="btn btn-primary btncate">${cate1[i]} </a> 
   </li>
 
 </ul>
@@ -72,11 +73,107 @@ console.log(name.length);
   
   }
   document.querySelector("#exmp").innerHTML = content;
+  document.querySelector("#cat1").style.display="none";
+  document.querySelector("#cat2").style.display="none";
+  document.querySelector("#cat3").style.display="none";
+}
+
+function cat2() {
+
+  let Catogrey1 = `Catogrey2`;
+  let c=0;
+  let cate = [];
+  const name = [];
+  let content = '';
+  let cate1=[];
+  for (var i = 0; i <= 9; i++) {
+
+    cate[i] = document.querySelectorAll(".btncate")[i].text.slice(0);
+
+
+    if (cate[i].charAt(8) === Catogrey1.charAt(8)) {
+
+      cate1[c]=cate[i];
+      name[c] = document.querySelectorAll(".name")[i].textContent.slice(0);
+      // console.log(name[i]);
+      // console.log(cate[i]);
+      c++;
+
+    }
+  }
+  
+console.log(name.length);
+
+  for (let i = 0; i <= name.length-1; i++) {
+    const firstLetters = name[i]
+    .split(' ')
+    .map(word => word[0])
+    .join('');
+    content += `
+    <div class="card" style="width: 25rem;margin:0 auto;top:6em;">
+<ul class="list-group list-group-flush">
+<li class="list-group-item"><span class="circle">${firstLetters}</span> <span class="name">  ${name[i]}   </span><a href="#" class="btn btn-primary btncate">${cate1[i]} </a> 
+  </li>
+
+</ul>
+</div>
+    `
+  
+  }
+  document.querySelector("#exmp").innerHTML = content;
+  document.querySelector("#cat1").style.display="none";
+  document.querySelector("#cat2").style.display="none";
+  document.querySelector("#cat3").style.display="none"
 }
 
 
+function cat3() {
+
+  let Catogrey1 = `Catogrey3`;
+  let c=0;
+  let cate = [];
+  const name = [];
+  let content = '';
+  let cate1=[];
+  for (var i = 0; i <= 9; i++) {
+
+    cate[i] = document.querySelectorAll(".btncate")[i].text.slice(0);
 
 
+    if (cate[i].charAt(8) === Catogrey1.charAt(8)) {
+
+      cate1[c]=cate[i];
+      name[c] = document.querySelectorAll(".name")[i].textContent.slice(0);
+      // console.log(name[i]);
+      // console.log(cate[i]);
+      c++;
+
+    }
+  }
+  
+console.log(name.length);
+
+  for (let i = 0; i <= name.length-1; i++) {
+    const firstLetters = name[i]
+    .split(' ')
+    .map(word => word[0])
+    .join('');
+    content += `
+    <div class="card" style="width: 25rem;margin:0 auto;top:6em;">
+<ul class="list-group list-group-flush">
+<li class="list-group-item"><span class="circle">${firstLetters}</span> <span class="name">  ${name[i]}   </span><a href="#" class="btn btn-primary btncate">${cate1[i]} </a> 
+  </li>
+
+</ul>
+</div>
+    `
+  
+  }
+  document.querySelector("#exmp").innerHTML = content;
+  document.querySelector("#cat1").style.display="none";
+  document.querySelector("#cat2").style.display="none";
+  document.querySelector("#cat3").style.display="none";
+}
 
 
 
