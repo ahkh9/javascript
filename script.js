717c8e4465bcc579f20520a6bbacfdf61bb8d46b
ahkh9 <100486910+ahkh9@users.noreply.github.com>
2022-10-06 19:07:49 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 async function load() {
-    let url = 'http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&pretty=true';
-    const obj = await (await fetch(url)).json();
-    console.log(obj);
+  let url = 'http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&pretty=true';
+  const obj = await (await fetch(url)).json();
+  console.log(obj);
 
-    const cat = ["Catogrey1", "Catogrey2", "Catogrey3"];
+  const cat = ["Catogrey1", "Catogrey2", "Catogrey3"];
 
 
-    let content = '';
+  let content = '';
 
-    obj.forEach(p => {
-        content += `
+  obj.forEach(p => {
+    content += `
           <div class="card" style="width: 25rem;margin:0 auto;top:6em;">
   <ul class="list-group list-group-flush">
     <li class="list-group-item"><span class="circle">${p.fname.charAt(0)}${p.lname.charAt(0)}</span> <span class="name">  ${p.fname}  ${p.lname} </span><a href="#" class="btn btn-primary btncate">${cat[Math.floor(Math.random() * 3)]} </a> 
@@ -19,47 +19,53 @@ async function load() {
   </ul>
 </div>
           `
-    });
+  });
 
-    document.querySelector("#exmp").innerHTML = content;
+  document.querySelector("#exmp").innerHTML = content;
 
 
-    // alert(obj[0].fname);
+  // alert(obj[0].fname);
 }
 
 load();
 
 
- function cat1(e) {
+function cat1() {
 
-    var Catogrey1 = `Catogrey1`;
-    let cate = [];
-    const name = [];
-    let content = '';
-   for (  let i = 0; i <= 9; i++) {
-  
-       cate[i] = document.querySelectorAll(".btncate")[i].text.slice(0);
+  let Catogrey1 = `Catogrey1`;
+  let c;
+  let cate = [];
+  const name = [];
+  let content = '';
+  for (var i = 0; i <= 9; i++) {
 
-
-        if (cate[i].charAt(8) === Catogrey1.charAt(8)) {
+    cate[i] = document.querySelectorAll(".btncate")[i].text.slice(0);
 
 
-            name[i] = document.querySelectorAll(".name")[i].textContent.slice(0);
-            console.log(name[i]);
+    if (cate[i].charAt(8) === Catogrey1.charAt(8)) {
+++c;
+
+      name[i] = document.querySelectorAll(".name")[i].textContent.slice(0);
+      console.log(name[i]);
+      console.log(cate[i]);
 
 
-            content += `
-                  <div class="card" style="width: 25rem;margin:0 auto;top:6em;">
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item"><span class="circle">${name[i].charAt(0)}</span> <span class="name">  ${name[i]}   </span><a href="#" class="btn btn-primary btncate">Category1 </a> 
-                </li>
-            
-          </ul>
-        </div>
-                  `
-            document.querySelector("#exmp").innerHTML = content;
-        }
     }
+  }
+
+  for (let i = 0; i <= name.length; i++) {
+    content += `
+    <div class="card" style="width: 25rem;margin:0 auto;top:6em;">
+<ul class="list-group list-group-flush">
+<li class="list-group-item"><span class="circle">${name[i]}</span> <span class="name">  ${name[i]}   </span><a href="#" class="btn btn-primary btncate">${cate[i]} </a> 
+  </li>
+
+</ul>
+</div>
+    `
+  
+  }
+  document.querySelector("#exmp").innerHTML = content;
 }
 
 
